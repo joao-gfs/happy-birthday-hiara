d6de14e1677e30a35f1cc24fcb46612adcf2d28c
João Guilherme F da Silva <joaoguilhermeferreira19@gmail.com>
2026-01-01 13:05:53 -0300

    switch adapter to static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,15 +9,15 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-            pages: 'build',
+			pages: 'build',
 			assets: 'build',
 			fallback: '404.html', // Importante para GitHub Pages
 			precompress: false,
 			strict: true
-        }),
-        paths: {
-            base: process.env.NODE_ENV === 'production' ? '/happy-birthday-hiara' : '',
-        }
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/happy-birthday-hiara' : '',
+		}
 	}
 };
 
